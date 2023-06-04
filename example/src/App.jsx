91ecc2dfa26e88d5baa20c4fddee3ba5b1fc8dc7
CurtisDAblorh <126522677+CurtisDAblorh@users.noreply.github.com>
2023-06-04 23:46:56 +0100

    Completed cart and Product page

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {Products, Navbar, Cart} from './components';
 import {commerce} from './lib/commerce';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -14,29 +14,56 @@ const App = () => {
 
 
   const fetchCart= async () => {
-    setCart(await commerce.cart.retrieve())
+    setCart(await commerce.cart.retrieve());
   }
 
   const handleAddToCart = async ( productId, quantity) => {
       const item = await commerce.cart.add(productId, quantity);
-      setCart(item)
+      setCart(item.cart);
   }
+
+  const handleUpdateCartQty = async ( productId, quantity) => {
+    const item = await commerce.cart.update(productId, {quantity});
+    setCart(item.cart);
+}
+
+
+const handleRemoveFromCart = async ( productId) => {
+  const item = await commerce.cart.remove(productId);
+  setCart(item.cart);
+}
+
+const handleEmptyCart = async ( ) => {
+  const item = await commerce.cart.empty();
+  setCart(item.cart);
+}
+
+
+
   useEffect(() => {
     fetchProducts();
     fetchCart();
   },[]);
 
 
-console.log(cart);
+
 
   return (
     <div>
-      
+     
       <Navbar totalItems={cart.total_items}/>
-      {/* <Products products={products} onAddToCart={handleAddToCart}/> */}
-      <Cart cart={cart}/>
+      <Routes>
+      <Route  path='/' element={<Products products={products} onAddToCart={handleAddToCart}/>}/>
+      <Route path='/cart' element={
+      <Cart
+       cart={cart}
+        handleUpdateCartQty ={handleUpdateCartQty}
+        handleRemoveFromCart ={handleRemoveFromCart}
+        handleEmptyCart={handleEmptyCart}
+      />}/>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
 export default App
--- example/src/components/Navbar/Navbar.jsx
+++ example/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Link, useMatch, useResolvedPath} from 'react-router-dom';
+import {Link, useLocation, useMatch, useResolvedPath,} from 'react-router-dom';
 import {AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography} from '@material-ui/core';
 import {ShoppingCart} from '@material-ui/icons';
 import logo from '../../assets/shop.png';
@@ -8,26 +8,32 @@ import useStyles from './styles';
 const Navbar = ({totalItems}) => {
 
   const classes = useStyles();
-  
+  const location = useLocation();
+
   return (
     
    <div>
+   
     <AppBar position="fixed" className={classes.appbar} color="inherit">
       <Toolbar>
-        <Typography variant="h6" className={classes.title} colour="inherit">
+        <Typography component = {Link} to="/"  variant="h6" className={classes.title} colour="inherit">
           <img src={logo} height="25px" alt="ebuyer" className={classes.image}/>
           ebuyer
         </Typography>
         <div className={classes.grow}/>
+
+        {location.pathname === '/'&& (
         <div className={classes.button}>
-          <IconButton aria-label="Show cart items" color="inherit">
+          <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
             <Badge badgeContent={totalItems} color="secondary">
               <ShoppingCart/>
             </Badge>
           </IconButton>
         </div>
+        )}
       </Toolbar>
     </AppBar>
+    
    </div>
   )
 }
