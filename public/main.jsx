e06c8d21e63cb2bec29225f907e22e584cab8019
CurtisDAblorh <126522677+CurtisDAblorh@users.noreply.github.com>
2023-07-02 03:08:46 +0100

    fix:main

--- public/main.jsx
+++ public/main.jsx
@@ -1,14 +1,12 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from '../src/App.jsx'
 import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-  <StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-    </StrictMode>
   ,
 )
